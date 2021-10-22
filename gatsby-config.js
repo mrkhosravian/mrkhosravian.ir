7cd2927ef19eb4bde64f0ce7a97727d25cd2d9af
Mohammad Reza Khosravian <jomanjo80@gmail.com>
2021-10-22 23:47:30 +0330

    Feature/add wordpress source (#2)
    * chore(package.json): add gatsby-source-wordpress package
    
    * feat(gatsby-config): add gatsby-source-wordpress configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -161,6 +161,33 @@ module.exports = {
         // siteSpeedSampleRate: 10,
         cookieDomain: "mrkhosravian.ir"
       }
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url:
+        // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
+          process.env.WPGRAPHQL_URL ||
+          `https://wp.mrkhosravian.ir/graphql`,
+        schema: {
+          //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
+          typePrefix: `Wp`
+        },
+        develop: {
+          //caches media files outside of Gatsby's default cache an thus allows them to persist through a cache reset.
+          hardCacheMediaFiles: true
+        },
+        type: {
+          Post: {
+            limit:
+              process.env.NODE_ENV === `development`
+                ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
+                50
+                : // and we don't actually need more than 5000 in production for this particular site
+                5000
+          }
+        }
+      }
     }
   ]
 };
