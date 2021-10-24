8699be0d1436cbdac66eb55ef41a88e1560f243f
mrkhosravian <jomanjo80@gmail.com>
2021-10-25 02:09:19 +0330

    fix(header): remove redundant border class

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,7 +4,7 @@ import HorizontalMenu from "../menus/horizontal-menu";
 const Header: FC = () => {
   return (
     <div
-      className={"py-3 border border-b-1 border-gray-300"}>
+      className={"py-3 border-b border-gray-300"}>
       <div className="max-w-5xl mx-auto flex justify-between">
         <h1 className={"w-64 font-bold"}>Mohammad Reza Khosravian</h1>
         <HorizontalMenu />
