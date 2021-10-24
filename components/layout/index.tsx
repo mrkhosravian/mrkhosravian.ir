4fd0b44a2ab674adb1f8e7ea8daece16e832d2f5
mrkhosravian <jomanjo80@gmail.com>
2021-10-25 02:09:38 +0330

    fix(header): update first page with wrapper layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,15 +1,17 @@
 import { FC, PropsWithChildren } from "react";
 import Cursor from "./cursor";
 import Header from "./header";
+import Footer from "./footer";
 
 interface LayoutProps {
 }
 
 const Layout: FC<PropsWithChildren<LayoutProps>> = (props) => {
   return (
-    <div className={"h-64"}>
+    <div className={"relative min-h-full"}>
       <Header />
       {props.children}
+      <Footer />
       <Cursor />
     </div>
   );
