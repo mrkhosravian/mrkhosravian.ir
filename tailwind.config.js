dfc9974d2abcd50615ad06260989d5e7065a0145
mrkhosravian <jomanjo80@gmail.com>
2021-10-24 23:45:29 +0330

    feat(tailwind): add tailwindcss to project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
