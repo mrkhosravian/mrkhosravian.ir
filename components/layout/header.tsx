4e563ac0742a67a1fe9e8e5eb8f11d9bb71b22e7
mrkhosravian <jomanjo80@gmail.com>
2021-10-25 04:02:41 +0330

    fix(header): make header position absolute

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,9 +4,9 @@ import HorizontalMenu from "../menus/horizontal-menu";
 const Header: FC = () => {
   return (
     <div
-      className={"py-3 border-b border-gray-300"}>
+      className={"py-3 border-b border-gray-300 absolute top-0 left-0 w-full"}>
       <div className="max-w-5xl mx-auto flex justify-between">
-        <h1 className={"w-64 font-bold"}>Mohammad Reza Khosravian</h1>
+        <h1 className={"w-64 font-bold text-gradient"}>Mohammad Reza Khosravian</h1>
         <HorizontalMenu />
         <a className={"w-64 text-right"} href="#">Contact</a>
       </div>
