da9d0bdf66b826c26138534141924e7fc0758976
mrkhosravian <jomanjo80@gmail.com>
2021-10-31 13:11:28 +0330

    feat(cursor): check for more mouse and touch events

--- components/layout/cursor.tsx
+++ components/layout/cursor.tsx
@@ -1,43 +1,80 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 
 const Cursor: FC = () => {
 
   const cursor = useRef<HTMLDivElement>(null);
+  const [show, setShow] = useState(false);
+
+  const mouseDownHandler = useCallback(() => {
+    if (cursor.current) {
+      cursor.current.classList.add("bg-gray-500");
+      cursor.current.classList.add("scale-75");
+      cursor.current.classList.remove("bg-gray-400");
+      cursor.current.classList.remove("opacity-60");
+    }
+  }, []);
+
+  const mouseUpHandler = useCallback(() => {
+    if (cursor.current) {
+      cursor.current.classList.add("bg-gray-400");
+      cursor.current.classList.add("opacity-60");
+      cursor.current.classList.remove("scale-75");
+      cursor.current.classList.remove("bg-gray-500");
+    }
+  }, []);
+
+  const cursorShow = useCallback(() => {
+    setShow(true);
+  }, []);
+
+  const cursorHide = useCallback(() => {
+    setShow(false);
+  }, []);
 
   useEffect(() => {
+
+    cursorShow();
+
     window.addEventListener("mousemove", (e) => {
       if (cursor.current) {
 
         const element = e.target as HTMLElement;
 
-        if (element && element.tagName.toLowerCase() === "a")
-          cursor.current.classList.add('bg-blue-700')
-        else
-          cursor.current.classList.remove('bg-blue-700')
+        if (element && element.tagName.toLowerCase() === "a") {
+          cursor.current.classList.add("bg-blue-700");
+        } else {
+          cursor.current.classList.remove("bg-blue-700");
+        }
 
         cursor.current.style.top = e.clientY + "px";
         cursor.current.style.left = e.clientX + "px";
       }
     });
 
-    window.addEventListener("mousedown", () => {
-      if (cursor.current) {
-        cursor.current.classList.add("bg-gray-500");
-        cursor.current.classList.add("scale-75");
-        cursor.current.classList.remove("bg-gray-400");
-        cursor.current.classList.remove("opacity-60");
-      }
-    });
+    window.addEventListener("mousedown", mouseDownHandler);
+    window.addEventListener("touchstart", mouseDownHandler);
 
-    window.addEventListener("mouseup", () => {
-      if (cursor.current) {
-        cursor.current.classList.add("bg-gray-400");
-        cursor.current.classList.add("opacity-60");
-        cursor.current.classList.remove("scale-75");
-        cursor.current.classList.remove("bg-gray-500");
-      }
-    });
-  }, []);
+    window.addEventListener("mouseup", mouseUpHandler);
+    window.addEventListener("touchend", cursorHide);
+
+    document.addEventListener("mouseenter", cursorShow);
+    document.addEventListener("mouseleave", cursorHide);
+
+
+    return () => {
+      window.removeEventListener("mousedown", mouseDownHandler);
+      window.removeEventListener("touchstart", mouseDownHandler);
+
+      window.removeEventListener("mouseup", mouseUpHandler);
+      window.removeEventListener("touchend", mouseUpHandler);
+
+      document.removeEventListener("mouseenter", cursorShow);
+      document.removeEventListener("mouseleave", cursorHide);
+    };
+  }, [mouseDownHandler, mouseUpHandler, cursorShow, cursorHide]);
+
+  if (!show)
+    return null;
 
   return (
     <div
