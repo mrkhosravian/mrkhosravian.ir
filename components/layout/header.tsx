d4003d4a43a1ab3d3e7b63479c9b20a64410339b
mrkhosravian <jomanjo80@gmail.com>
2021-11-13 21:16:29 +0330

    feat(hamburger): add hamburger menu

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,13 +2,14 @@ import { FunctionComponent } from "react";
 import HorizontalMenu from "../menus/horizontal-menu";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
+import HamburgerMenu from "../menus/hamburger-menu";
 
 const Header: FunctionComponent = () => {
   const { t } = useTranslation();
 
   return (
     <div
-      className={"py-3 px-5 xl:px-0 border-b border-gray-300 dark:border-gray-600 dark:bg-gray-900 fixed top-0 left-0 w-full bg-white backdrop-blur-3xl z-10"}>
+      className={"py-3 px-5 xl:px-0 border-b border-gray-300 dark:border-gray-600 dark:bg-gray-900 fixed top-0 left-0 w-full bg-white z-20"}>
       <div className="max-w-5xl mx-auto flex justify-between">
         <h1 className={"w-64 font-bold text-gradient"}>
           <Link href={"/"}>
@@ -16,6 +17,7 @@ const Header: FunctionComponent = () => {
           </Link>
         </h1>
         <HorizontalMenu />
+        <HamburgerMenu />
         {/*<a className={"w-64 text-right rtl:text-left hidden md:block"} href="#">{t('Contact')}</a>*/}
       </div>
     </div>
