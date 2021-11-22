e7e3db180bce0ae7605d7d057aaa1e7a6e09213d
mrkhosravian <jomanjo80@gmail.com>
2021-11-22 12:10:09 +0330

    feat(analytics): add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "next-themes";
 import { useRouter } from "next/router";
 import moment from "moment-jalaali";
 import "../styles/themes/atom-onedark.min.css";
+import Script from "next/script";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -15,6 +17,19 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider attribute="class">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   );
