7a2b10bfb505daf72c287d94da7c83932dc7f9ca
Mohammad Reza Khosravian <jomanjo80@gmail.com>
2022-05-23 14:22:57 +0430

    feat: use next-image-zoom instead of ZoomableImg.tsx

--- components/gallery/ImageGallery.tsx
+++ components/gallery/ImageGallery.tsx
@@ -1,5 +1,6 @@
 import { FC, PropsWithChildren } from "react";
-import ZoomableImg from "../partials/ZoomableImg";
+import Zoom from "next-image-zoom";
+import { useTheme } from "next-themes";
 
 interface Props {
   images: {
@@ -9,12 +10,22 @@ interface Props {
 }
 
 export const ImageGallery: FC<PropsWithChildren<Props>> = (props) => {
+
+  const theme = useTheme();
+
   return (
     <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5"}
-         style={{ gridTemplateRows: "masonry"}}>
+         style={{ gridTemplateRows: "masonry" }}>
       {
         props.images.map((image, i) => {
-          return <ZoomableImg key={i} src={image.src} alt={image.alt} />;
+          return (
+            <div className={"w-full"} style={{ aspectRatio: "1 / 1" }} key={i}>
+              <Zoom src={image.src} alt={image.alt} layout={"fill"}
+                    objectFit={"contain"} zoomPercentage={100}
+                    backgroundColor={theme.theme === "light" ? "white" : "black"}
+                    backgroundOpacity={1} />
+            </div>
+          );
         })
       }
     </div>
